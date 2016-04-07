fe8717a578dd66dfaf5978088cdecb4daaa1187a
Daniel Holmlund <agnathan@gmail.com>
2016-04-07 13:25:40 -0700

    Initial Commit of Virtual Sensor

---
+++ virtual-sensor.js
@@ -0,0 +1,73 @@
+/*
+ * Author: Daniel Holmlund <daniel.w.holmlund@Intel.com>
+ * Copyright (c) 2015 Intel Corporation.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+var config = require("./config.json");
+var mqtt = require('mqtt');
+var _ = require('lodash');
+var client  = mqtt.connect(config.mqtt.url);
+
+var tempSensorName = "temperature";
+var tempSensorTopic = "sensors/" + tempSensorName + "/data";
+
+var lightSensorName = "light";
+var lightSensorTopic = "sensors/" + lightSensorName + "/data";
+
+client.on('connect', function () {
+    console.log("Connected to the MQTT server on " + config.mqtt.url);
+});
+
+function getRandomTemp(min, max) {
+    // Returns a random number between min (inclusive) and max (exclusive)
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+setInterval(function() {
+    var temp = getRandomTemp(17, 30);
+
+    var lightTemp = getRandomTemp(200, 1000);
+    var current_time = (new Date).getTime();
+
+    var str = '{"sensor_id": "'
+            + tempSensorName
+            + '", "value": "'
+            + temp
+            + '", "timestamp":"'
+            + current_time +'"}';
+
+    console.log(str);
+
+
+    client.publish(tempSensorTopic, str);
+
+    var str = '{"sensor_id": "'
+            + lightSensorName
+            + '", "value": "'
+            + lightTemp
+            + '", "timestamp":"'
+            + current_time +'"}';
+
+//    console.log(str);
+//    client.publish(lightSensorTopic, str);
+
+
+}, config.interval);

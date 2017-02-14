2c6fcb0ea302ed3d6e9d53eff729f1f4c301fdfe
Daniel Holmlund <agnathan@gmail.com>
2017-02-14 09:26:26 -0800

    A little cleanup. Added sample certificates to simplify for developers.

--- virtual-sensor-secure.js
+++ virtual-sensor-secure.js
@@ -21,18 +21,19 @@
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-var config = require("./config.json");
-var _ = require('lodash');
-
 var mqtt = require('mqtt');
 var fs = require('fs');
-var KEY = fs.readFileSync('/etc/mosquitto/certs/server.key');
-var CERT = fs.readFileSync('/etc/mosquitto/certs/server.crt');
-var TRUSTED_CA_LIST = [fs.readFileSync('/etc/mosquitto/ca_certificates/ca.crt')];
- 
+
+// Load application specific configurations from config.json
+var config = require("./config.json");
+var KEY = fs.readFileSync(config.KEY_FILE);
+var CERT = fs.readFileSync(config.CERT_FILE);
+var TRUSTED_CA_LIST = [fs.readFileSync(config.TRUSTED_CA_LIST_FILE)];
+
 var PORT = 8883;
 var HOST = 'localhost';
- 
+
+// Setup MQTT-TLS options
 var options = {
   port: PORT,
   host: HOST,
@@ -40,22 +41,19 @@ var options = {
   protocolId: 'MQIsdp',
   keyPath: KEY,
   certPath: CERT,
-  rejectUnauthorized : false, 
+  rejectUnauthorized : false,
   //The CA list will be used to determine if server is authorized
   ca: TRUSTED_CA_LIST,
   secureProtocol: 'TLSv1_method',
   protocolVersion: 3
 };
- 
+
 //var client  = mqtt.connect(config.mqtt.url);
 //var client = mqtt.createSecureClient(options);
 var client = mqtt.connect(options);
 
 var tempSensorName = "temperature";
 var tempSensorTopic = "sensors/" + tempSensorName + "/data";
-
-var lightSensorName = "light";
-var lightSensorTopic = "sensors/" + lightSensorName + "/data";
 
 client.on('connect', function () {
     console.log("Connected to the MQTT server on " + config.mqtt.url);
@@ -83,16 +81,5 @@ setInterval(function() {
 
 
     client.publish(tempSensorTopic, str);
-
-    var str = '{"sensor_id": "'
-            + lightSensorName
-            + '", "value": "'
-            + lightTemp
-            + '", "timestamp":"'
-            + current_time +'"}';
-
-//    console.log(str);
-//    client.publish(lightSensorTopic, str);
-
 
 }, config.interval);

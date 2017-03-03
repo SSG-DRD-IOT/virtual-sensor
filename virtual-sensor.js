9582cfda8d4b1e23bbb4190bf53dc7814638f0d3
Daniel Holmlund <agnathan@gmail.com>
2017-03-02 18:20:26 -0600

    Added command line options to define sensor parameters
    The help text for the virtual sensor now looks like this.
    
    A Virtual Sensor Application
    
      Generates sensor data traffic over MQTT
    
    Main options
    
      -h, --help              Display this usage guide.
    
      -v, --verbose           Display extra information
    
      -d, --debug             Display debugging information
    
      --delay number          the delay between sensor readings in
    
                              milliseconds. Default is 1000ms
    
      -i, --hostname string   the hostname or IP address of the MQTT broker
    
      -n, --name string       the id of the sensor
    
      -p, --port number       the port number to connect to the MQTT broker
    
      -t, --timeout ms        Stop emitting sensors readings after timeout
    
                              number of milliseconds
    
      --digital               Specifies that the sensor will be digital
    
      --analog                Specifies that the sensor will be analog
    
    Analog Sensor
    
      --min number   the minimum value emitted
      --max number   the maximun value emitted

--- virtual-sensor.js
+++ virtual-sensor.js
@@ -31,29 +31,159 @@
  // Require MQTT and setup the connection to the broker
  var mqtt = require('mqtt');
 
+ // Libary to parse command line arguments
+ const commandLineArgs = require('command-line-args')
+ const getUsage = require('command-line-usage');
+
+// Command line argument definitions
+ const optionDefinitions = [
+   {
+     name: 'help',
+     description: 'Display this usage guide.',
+     alias: 'h',
+     type: Boolean,
+     group: 'main'
+   },
+   {
+     name: 'verbose',
+     description: 'Display extra information',
+     alias: 'v',
+     type: Boolean,
+     group: 'main'
+   },
+   {
+     name: 'debug',
+     description: 'Display debugging information',
+     alias: 'd',
+     type: Boolean,
+     group: 'main'
+   },
+   {
+     name: 'delay',
+     description: 'the delay between sensor readings in milliseconds. Default is 1000ms',
+     type: Number,
+     defaultValue: 1000,
+     group: 'main'
+   },
+   {
+     name: 'hostname',
+     description: 'the hostname or IP address of the MQTT broker',
+     type: String,
+     alias: 'i',
+     defaultValue: "localhost",
+     group: 'main'
+   },
+   {
+     name: 'name',
+     description: 'the id of the sensor',
+     type: String,
+     alias: 'n',
+     defaultValue: "temperature",
+     group: 'main'
+   },
+   {
+     name: 'port',
+     description: 'the port number to connect to the MQTT broker',
+     type: Number,
+     alias: 'p',
+     defaultValue: 1883,
+     group: 'main'
+   },
+   {
+     name: 'timeout',
+     description: 'Stop emitting sensors readings after timeout number of milliseconds',
+     alias: 't',
+     type: Number,
+     typeLabel: '[underline]{ms}',
+     group: 'main'
+   },
+   {
+     name: 'digital',
+     description: 'Specifies that the sensor will be digital',
+     type: Boolean,
+     group: 'main'
+   },
+   {
+     name: 'analog',
+     description: 'Specifies that the sensor will be analog',
+     type: Boolean,
+     group: 'main'
+   },
+   {
+     name: 'min',
+     description: 'the minimum value emitted',
+     type: Number,
+     defaultValue: 17,
+     group: 'analog'
+   },
+   {
+     name: 'max',
+     description: 'the maximun value emitted',
+     type: Number,
+     defaultValue: 30,
+     group: 'analog'
+   }
+ ]
+
+ const sections = [
+   {
+     header: 'A Virtual Sensor Application',
+     content: 'Generates sensor data traffic over MQTT'
+   },
+   {
+     header: 'Main options',
+     optionList: optionDefinitions,
+     group: [ 'main', 'input' ]
+   },
+   {
+     header: 'Analog Sensor',
+     optionList: optionDefinitions,
+     group: 'analog'
+   }
+ ];
+
+// Parse the command line arguments
+const options = commandLineArgs(optionDefinitions);
+// Print the help text
+if (options.main.help) {
+  console.log(getUsage(sections))
+    process.exit();
+}
+
+// Print the options use if in debug mode
+if(options.main.debug) {
+  console.log(options);
+}
+
+var min = 0;
+var max = 1;
+if (options.main.analog) {
+  min = options.main.min;
+  max = options.main.max;
+}
+
 // Create an MQTT client named client that is
 // conncect to the *config.mqtt.url* value
-var client  = mqtt.connect(config.mqtt.url);
+var client  = mqtt.connect("mqtt://" + options.main.hostname + ":" + options.main.port + "/");
 
 // Log virtual-sensor as started
-console.log(chalk.bold.yellow("Virtual Sensor is started and attempting to connect to an MQTT broker"));
+info("Virtual Sensor is started and attempting to connect to an MQTT broker");
 
 // Create variables for the sensor name and topic
-var tempSensorName = "temperature";
-var tempSensorTopic = "sensors/" + tempSensorName + "/data";
+var topic = "sensors/" + options.main.name + "/data";
 
 // On the client connect event run a function
 // to log the event to the console
 client.on('connect', function () {
-    console.log(chalk.bold.yellow("Connected to the MQTT server on " + config.mqtt.url));
-
+    info("Connected to the MQTT server on " + "mqtt://" + options.main.hostname + ":" + options.main.port + "/");
+    info("Publishing sensors reading to " + topic)
     /*
       Runs a function to emit a temperature sensor value
       every *config.interval* milliseconds
     */
     setInterval(function() {
-        // Get a random temperature integer
-        var temp = getRandomTemp(17, 30);
+        // Get a random integer value between a min and a max
+        var value = randomIntBetween(min, max);
 
         // Get the current time
         var current_time = (new Date).getTime();
@@ -64,8 +194,8 @@ client.on('connect', function () {
           reading has a "sensor_id", "value" and "timestamp"
         */
         var json = {
-          sensor_id : tempSensorName,
-          value : temp,
+          sensor_id : options.main.name,
+          value : value,
           timestamp : current_time
         };
 
@@ -75,20 +205,37 @@ client.on('connect', function () {
         // Log the string to the console
         console.log(str);
 
-        // Publish the temperature reading string on the MQTT topic
-        client.publish(tempSensorTopic, str);
-    }, config.interval);
+        // Publish the sensor reading string on the MQTT topic
+        client.publish(topic, str);
+    }, options.main.delay);
+
+    // If the timeout options is defined then emit sensor data for timeout
+    // number of milliseconds
+
+    if (options.main.timeout != undefined) {
+      info("Timeout set for " + options.main.timeout + "ms")
+      setInterval(function() {
+        process.exit();
+      }, options.main.timeout)
+    }
 });
 
 // MQTT error function - Client unable to connect
 client.on('error', function () {
-    console.log(chalk.bold.yellow("Unable to connect to MQTT server"));
+    info("Unable to connect to MQTT server");
     process.exit();
 });
 
 /*
-  getRandomTemp: returns a random integer between min and max
+  randomIntBetween: returns a random integer between min and max
 */
-function getRandomTemp(min, max) {
+function randomIntBetween(min, max) {
   return Math.round(Math.random() * (max - min) + min);
+}
+
+function info(str) {
+  // Log virtual-sensor as started
+  if (options.main.verbose == true) {
+    console.log(chalk.bold.yellow(str));
+  }
 }

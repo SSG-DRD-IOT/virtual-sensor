da957edcc3df6a9cd26324c8a2f76358ec7b4795
Daniel Holmlund <agnathan@gmail.com>
2017-06-22 19:31:57 -0700

    Fixing problems from spliting the cmdline args into a different file

--- virtual-sensor.js
+++ virtual-sensor.js
@@ -28,9 +28,166 @@ var chalk = require('chalk');
 // Require MQTT and setup the connection to the broker
 var mqtt = require('mqtt');
 
-// Parse the command line arguments
-const options = require('./commandline.js');
+// Libary to parse command line arguments
+const commandLineArgs = require('command-line-args')
+const getUsage = require('command-line-usage');
 
+// Command line argument definitions
+const optionDefinitions = [{
+        name: 'help',
+        description: 'Display this usage guide.',
+        alias: 'h',
+        type: Boolean,
+        group: 'main'
+    },
+    {
+        name: 'silent',
+        description: 'Suppresses extra information',
+        alias: 's',
+        type: Boolean,
+        group: 'main'
+    },
+    {
+        name: 'debug',
+        description: 'Display debugging information',
+        alias: 'd',
+        type: Boolean,
+        group: 'main'
+    },
+    {
+        name: 'tls',
+        description: 'Use MQTT-TLS. MQTT encryption over TLS.',
+        type: Boolean,
+        group: 'encryption'
+    },
+    {
+        name: 'key',
+        description: 'the client key file',
+        type: String,
+        defaultValue: "certs/server.key",
+        group: 'encryption'
+    },
+    {
+        name: 'cert',
+        description: 'the client certificate file',
+        type: String,
+        defaultValue: "certs/server.crt",
+        group: 'encryption'
+    },
+    {
+        name: 'ca',
+        description: 'the client ca file',
+        type: String,
+        defaultValue: "certs/ca.crt",
+        group: 'encryption'
+    },
+    {
+        name: 'delay',
+        description: 'the delay between sensor readings in milliseconds. Default is 1000ms',
+        type: Number,
+        alias: 'l',
+        defaultValue: 1000,
+        group: 'main'
+    },
+    {
+        name: 'hostname',
+        description: 'the hostname or IP address of the MQTT broker',
+        type: String,
+        alias: 'i',
+        defaultValue: "localhost",
+        group: 'connection'
+    },
+    {
+        name: 'name',
+        description: 'the id of the sensor',
+        type: String,
+        alias: 'n',
+        defaultValue: "temperature",
+        group: 'sensor'
+    },
+    {
+        name: 'port',
+        description: 'the port number to connect of the MQTT broker',
+        type: Number,
+        alias: 'p',
+        group: 'connection'
+    },
+    {
+        name: 'timeout',
+        description: 'Stop emitting sensors readings after timeout number of milliseconds',
+        alias: 't',
+        type: Number,
+        typeLabel: '[underline]{ms}',
+        group: 'main'
+    },
+    {
+        name: 'digital',
+        description: 'Specifies that the sensor will be digital',
+        type: Boolean,
+        group: 'sensor'
+    },
+    {
+        name: 'analog',
+        description: 'Specifies that the sensor will be analog',
+        type: Boolean,
+        defaultValue: true,
+        group: 'sensor'
+    },
+    {
+        name: 'min',
+        description: 'the minimum value emitted',
+        type: Number,
+        defaultValue: 17,
+        group: 'analog'
+    },
+    {
+        name: 'max',
+        description: 'the maximun value emitted',
+        type: Number,
+        defaultValue: 30,
+        group: 'analog'
+    }
+]
+
+const sections = [{
+        header: 'A Virtual Sensor Application',
+        content: 'Generates sensor data traffic over MQTT and MQTT-TLS'
+    },
+    {
+        header: 'Main options',
+        optionList: optionDefinitions,
+        group: ['main', 'input']
+    },
+    {
+        header: 'Connection options',
+        optionList: optionDefinitions,
+        group: ['connection']
+    },
+    {
+        header: 'Encryption options',
+        optionList: optionDefinitions,
+        group: ['encryption']
+    },
+    {
+        header: 'Sensor Options',
+        optionList: optionDefinitions,
+        group: 'sensor'
+    },
+    {
+        header: 'Analog Sensor Options',
+        optionList: optionDefinitions,
+        group: 'analog'
+    }
+    // ,
+    // {
+    //     header: 'Digital Sensor Options',
+    //     optionList: optionDefinitions,
+    //     group: 'digital'
+    // }
+];
+
+// Parse the command line arguments
+const options = commandLineArgs(optionDefinitions);
 // Print the help text
 if (options.main.help) {
     console.log(getUsage(sections))
